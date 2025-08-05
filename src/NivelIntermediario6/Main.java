5b352e9822c2df531b2ca9327d8fc1ae9daa5cf8
LucazsLO <lucaslure2.0@gmail.com>
2025-08-04 22:12:42 -0300

    docs: Studying List

---
+++ src/NivelIntermediario6/Main.java
@@ -0,0 +1,33 @@
+package NivelIntermediario6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //array
+        String [] ninjaArray = new String[3];
+        ninjaArray [0] = "Naruto";
+        ninjaArray [1] = "Sasuke";
+        ninjaArray [2] = "Sakura";
+
+        for (int i= 0; i<ninjaArray.length;i++){
+        System.out.println("Print array " + ninjaArray[i]);
+        }
+
+        //List
+        List <String> ninjaList = new ArrayList<>();
+        ninjaList.add("Naruto Uzumaki");
+        ninjaList.add("Sasuke Uchiha");
+        ninjaList.add("Gaara");
+        ninjaList.add("Tobi");
+
+        System.out.println("Print List" + ninjaList);
+
+        ninjaList.set(3, "Kakashi");
+        System.out.println("Print List" + ninjaList);
+
+
+    }
+}
